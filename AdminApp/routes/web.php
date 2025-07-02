673bd901f38757a8f17fc9beb5772246d1432cf1
Mahadi Hasan <m.mahadi228@gmail.com>
2025-07-02 19:13:59 +0600

    Setting Page Den

--- AdminApp/routes/web.php
+++ AdminApp/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\SSLCommerzCredentialController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return inertia('Home');
-});
+
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('page.dashboard');
+Route::resource('/settings', SSLCommerzCredentialController::class);
+
+    
